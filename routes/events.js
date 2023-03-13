6f6070954d3cff36a9cd5247aa72a4ccc9e32a32
Ruben Rodriguez <rrodriguez@opentrends.net>
2023-03-13 18:40:03 +0100

    Backend 1.0 terminado

---
+++ routes/events.js
@@ -0,0 +1,46 @@
+/**
+ * Event Routes
+ * /api/events
+ */
+
+const { Router } = require('express');
+const { check } = require('express-validator');
+const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require('../controllers/events');
+const { isDate } = require('../helpers/isDate');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
+
+const router = Router();
+
+router.use(validarJWT);
+
+// Todas tienen que pasar por la validación del JWT
+// Obtener eventos
+router.get('/', getEventos);
+
+// Crear un nuevo evento
+router.post(
+    '/',
+    [
+        check('title', 'El título es obligatorio').not().isEmpty(),
+        check('start', 'Fecha de inicio es obligatoria').custom(isDate),
+        check('end', 'Fecha de finalización es obligatoria').custom(isDate),
+        validarCampos
+    ],
+    crearEvento);
+
+// Actualizar evento
+router.put(
+    '/:id',
+    [
+        check('title', 'El título es obligatorio').not().isEmpty(),
+        check('start', 'Fecha de inicio es obligatoria').custom(isDate),
+        check('end', 'Fecha de finalización es obligatoria').custom(isDate),
+        validarCampos
+    ],
+    actualizarEvento);
+
+// Borrar evento
+router.delete('/:id', eliminarEvento);
+
+module.exports = router;
